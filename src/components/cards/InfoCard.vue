96fcd2d0b170f769ee5bcdb7b7e459a28434dc3b
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-04-28 14:37:58 +0200

    [1.4.4] fix: infocard content not visible in dark mode

--- src/components/cards/InfoCard.vue
+++ src/components/cards/InfoCard.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useTheme } from 'vuetify/lib/framework.mjs';
 import BaseCard from '@/components/base/BaseCard.vue';
 
 const props = defineProps({
@@ -18,21 +16,11 @@ const props = defineProps({
     type: String,
     default: 'primary'
   }
-})
-
-const isDark = computed(() => {
-  return useTheme().global.name.value === 'dark'
-})
-
-const getOutline = computed<boolean>(() => {
-  return isDark.value ? true : false
-})
+});
 </script>
 
 <template>
-  <base-card
-    :outlined="getOutline"
-    :color="props.color">
+  <base-card :color="props.color">
     <v-card-text>
       <span
         v-if="props.htmlIsh"
